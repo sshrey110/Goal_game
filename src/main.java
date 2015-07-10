ecea248f4e8a4e2a9ecfcdf99250a6bce1d5f198
shrey <sshrey110@gmail.com>
2015-07-10 17:13:06 +0530

    hh

---
+++ src/main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+/**
+ * Created by SANDEEP on 7/10/2015.
+ */
+public class main {
+    static Scanner key = new Scanner(System.in);
+    public static double digit;
+    public static void main(String[] args) {
+        System.out.println("Enter any number");
+        int input = key.nextInt();
+        digit = Math.pow(2, input);
+        for (int i = 0; i < digit; i++) {
+            String x1 = decimalToBinary(i);
+            System.out.println(x1);
+        }
+    }
+    public static String decimalToBinary(int decimal)
+    {
+        String binary = "";
+        while (decimal != 0)
+        {
+            binary = (decimal % 2) + binary;
+            decimal = decimal / 2;
+        }
+        return binary;
+    }
+    public void ka(int len,String ps)
+    {
+        if(len==1){
+            System.out.println(ps+"0");
+            System.out.println(ps+"0");
+            return;
+        }
+        ka(len-1,"0");
+        ka(len-1,"1");
+    }
+}
+
+
+
